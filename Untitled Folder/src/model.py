4978d39a536a6acefd212d83bdcafffc5dd94840
wonchul-kim <onedang22@gmail.com>
2020-03-05 11:16:39 +0900

    alexnet with tensorflow

--- Untitled Folder/src/model.py
+++ Untitled Folder/src/model.py
@@ -4,4 +4,21 @@ def weights(shape, stddev=0.01):
     return tf.get_variable("weights", shape, tf.float32, \
                         tf.random_normal_initializer(mean=0.0, stddev=stddev))
 
-def biases(shape, )
+def biases(shape, value=1.0):
+    return tf.get_variable('biases', shape, tf.float32, \
+                        tf.constant_initializer(value=vlaue))
+
+def conv2d(x, W, stride, padding='SAME'):
+    '''
+        x: tf.Tensor (N, H, W, C)
+        W: tf.Tensor (fh, fw, ic, oc)
+    '''
+
+    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding=padding)
+
+def max_pooling(x, width, stride, padding='SAME'):
+    return tf.nn.max_pool(x, ksize=[1, width, width, 1], \
+                strides=[1, stride, stride, 1], padding=padding)
+
+
+def conv_layer
