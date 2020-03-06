7ba17f8e47ee145993ed9b4fdbd42bc22fbb6d58
wonchul-kim <onedang22@gmail.com>
2020-03-06 11:38:09 +0900

    alexnet_tf

--- alexnet/alexnet_tf/src/model.py
+++ alexnet/alexnet_tf/src/model.py
@@ -9,16 +9,17 @@ def biases(shape, value=1.0):
                         tf.constant_initializer(value=vlaue))
 
 def conv2d(x, W, stride, padding='SAME'):
-    '''
-        x: tf.Tensor (N, H, W, C)
-        W: tf.Tensor (fh, fw, ic, oc)
-    '''
-
     return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding=padding)
 
 def max_pooling(x, width, stride, padding='SAME'):
     return tf.nn.max_pool(x, ksize=[1, width, width, 1], \
                 strides=[1, stride, stride, 1], padding=padding)
 
+def add_conv2d(x, filter_width, stride, nb_output_channel, padding='SAME'):
+    nb_intput_channel = int(x.get_shape()[-1])
+    w = weights([filter_width, filter_width, nb_input_channel, nb_output_channel], stddev=0.01)
+    b = biases([nb_output_channel], value=0.1)
 
-def conv_layer
+    return conv2d(x, w, stride, padding=padding) + b
+
+def add_fc(x, output_)
